4ecbffe27f69b9acbc398ed58a6a6b21544554d8
Warren Seto <nextseto@gmail.com>
2016-12-17 17:42:41 -0500

    Added Projects from Fall 2016 (Phase 1)
    - Added the source for the test bench and the modules for the 64-bit
    adder

---
+++ Project 4 - 64-Bit Adder/behavioral_adder_64/behavioral_full_adder_test.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+/*
+	Group Members: Nikita Eisenhauer and Warren Seto
+
+	Lab Name: Adder Design 
+	Design Description: Verilog Test Fixture to test the behavioral 64-bit adder
+*/
+
+module behavioral_full_adder_test;
+
+	// Inputs
+	reg [63:0] A;
+	reg [63:0] B;
+
+	// Outputs
+	wire [64:0] SUM;
+
+	// Instantiate two counter variables for the test loop
+  	integer count;
+  	integer count2;
+
+	// Instantiate the Unit Under Test (UUT)
+	behavioral_64_full_adder uut 
+	(
+		.A(A), 
+		.B(B), 
+		.SUM(SUM)
+	);
+	
+  	initial begin
+		
+		$monitor("%d + %d = %d", A, B, SUM);
+		
+		// Iterate through all possible combination of 0-32
+		count = 0;
+		count2 = 0;
+
+		A = 0;
+		B = 0;
+		
+		// Loops over the possible combinations for the inputs A and B
+		for (count = 0; count <= 32; count = count + 1) begin
+		{A} = count;
+			for (count2 = 0; count2 <= 32; count2 = count2 + 1) begin
+				{B} = count2;
+				#1;
+			end
+		end
+
+	end
+	
+	initial #4000 $finish; // The test will run for a total interval of 4000 nanoseconds
+endmodule
---
+++ Project 4 - 64-Bit Adder/ripple_adder_64/ripple_adder_64_test.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+/*
+	Group Members: Nikita Eisenhauer and Warren Seto
+
+	Lab Name: Adder Design 
+	Design Description: Verilog test fixture to test the 64-bit ripple adder
+*/
+
+module ripple_adder_64_test;
+
+	// Inputs
+	reg [63:0] A;
+	reg [63:0] B;
+
+	// Outputs
+	wire [63:0] SUM;
+	wire CARRY;
+	
+	// Instantiate two counter variables for the test loop
+  	integer count;
+  	integer count2;
+
+	// Instantiate the Unit Under Test (UUT)
+	ripple_adder_64 uut
+	(
+		.A(A),
+		.B(B),
+		.SUM(SUM),
+		.CARRY(CARRY)
+	);
+	
+	initial begin
+		
+		$monitor("%d + %d = %d and carry %d", A, B, SUM, CARRY);
+
+		// Iterate through all possible combination of 0-32
+		count = 0;
+		count2 = 0;
+
+		A = 0;
+		B = 0;
+		
+		// Loops over the possible combinations for the inputs A and B
+		for (count = 0; count <= 32; count = count + 1) begin
+
+		{A} = count;
+			
+			for (count2 = 0; count2 <= 32; count2 = count2 + 1) begin
+				{B} = count2;
+				#1;
+			end
+		end
+
+	end
+	
+	initial #4000 $finish; // The test will run for a total interval of 4000 nanoseconds
+endmodule
